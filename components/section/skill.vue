2a6149b65d445397788e449fb1637faf6e98ead6
Poom <poompongphun@gmail.com>
2021-05-24 22:21:19 +0700

    update animation

--- components/section/skill.vue
+++ components/section/skill.vue
@@ -1,41 +1,44 @@
 <template>
-  <div v-intersect="onIntersect">
+  <div
+    v-intersect="onIntersect"
+    class="my-10 scroll-up"
+    :class="{ 'scroll-up-enter': isShow }"
+  >
     <div class="py-5 text-center">
       <h1>
         {{ $t('welcome').skill }} <v-chip>{{ skillShow.length }}</v-chip>
       </h1>
     </div>
-    <v-scroll-y-reverse-transition>
-      <v-slide-group
-        v-show="isShow"
-        v-model="model"
-        class="custom-slide background rounded-xl mx-5"
-        show-arrows
-      >
-        <v-slide-item v-for="(skill, n) in skillShow" :key="n">
-          <v-tooltip bottom>
-            <template #activator="{ on, attrs }">
-              <v-card
-                :class="`my-${margin} mx-2 pa-5 rounded-xl text-center elevation-0`"
-                :height="size"
-                :width="size"
-                v-bind="attrs"
-                v-on="on"
-              >
-                <v-img
-                  class="mx-auto"
-                  width="100%"
-                  height="100%"
-                  :src="skill.img"
-                  contain
-                ></v-img>
-              </v-card>
-            </template>
-            <span>{{ skill.name }}</span>
-          </v-tooltip>
-        </v-slide-item>
-      </v-slide-group>
-    </v-scroll-y-reverse-transition>
+    <v-slide-group
+      v-model="model"
+      class="custom-slide background rounded-xl mx-5 pa-1"
+      show-arrows
+    >
+      <v-slide-item v-for="(skill, n) in skillShow" :key="n">
+        <v-tooltip bottom>
+          <template #activator="{ on, attrs }">
+            <v-card
+              :ref="`skill-${n}`"
+              class="my-1 mx-1 pa-5 rounded-xl text-center elevation-0 slide-left"
+              :class="{ 'slide-left-enter': skill.show }"
+              :height="size"
+              :width="size"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-img
+                class="mx-auto"
+                width="100%"
+                height="100%"
+                :src="skill.img"
+                contain
+              ></v-img>
+            </v-card>
+          </template>
+          <span>{{ skill.name }}</span>
+        </v-tooltip>
+      </v-slide-item>
+    </v-slide-group>
   </div>
 </template>
 
@@ -60,23 +63,34 @@ export default {
         ? '175'
         : '150'
     },
-    margin() {
-      return this.$vuetify.breakpoint.mdAndUp ? '4' : '3'
+  },
+  watch: {
+    isShow(val) {
+      if (val) {
+        for (let i = 0; i < this.skills.length; i++) {
+          ;((index) => {
+            setTimeout(() => {
+              this.skillShow.push(this.skills[i])
+              setTimeout(() => {
+                this.skillShow[i].show = true
+
+                // fix bug if dont use this last array will not show
+                if (i === this.skills.length - 1) {
+                  this.$refs[`skill-${i}`][0].$el.classList.value =
+                    this.$refs[`skill-${i}`][0].$el.classList.value +
+                    ' slide-left-enter'
+                }
+              }, 60)
+            }, i * 200)
+          })(i)
+        }
+      }
     },
   },
   methods: {
     onIntersect(entries, observer) {
       if (!this.isShow) {
-        setTimeout(() => {
-          this.isShow = entries[0].isIntersecting
-          for (let i = 0; i < this.skills.length; i++) {
-            ;((index) => {
-              setTimeout(() => {
-                this.skillShow.push(this.skills[i])
-              }, i * 100)
-            })(i)
-          }
-        }, 200)
+        this.isShow = entries[0].isIntersecting
       }
     },
   },
